bac36d361d888c76daf675baefb09fb84e5845d2
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-01-12 13:51:08 -0300

    docs: add API documentation

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UniversityAlreadyExistsInterceptor } from './universities/errors/interceptors/university-already-exists.interceptor';
 import { UniversityNotFoundInterceptor } from './universities/errors/interceptors/university-not-found.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Bis2Bis - Challenge')
+    .setDescription('University Management API')
+    .setVersion('0.0.1')
+    .addTag('Universities')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   // Global pipes
   app.useGlobalPipes(new ValidationPipe());
 
--- src/universities/dto/create-university.dto.ts
+++ src/universities/dto/create-university.dto.ts
@@ -1,23 +1,30 @@
 import { IsArray, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUniversityDto {
+  @ApiProperty({ example: '["http://teste.com"]' })
   @IsArray()
   @IsString({ each: true })
   web_pages: string[];
 
+  @ApiProperty({ example: 'RS' })
   @IsString()
   readonly state_province: string;
 
+  @ApiProperty({ example: 'BR' })
   @IsString()
   readonly alpha_two_code: string;
 
+  @ApiProperty({ example: 'Brazil' })
   @IsString()
   readonly country: string;
 
+  @ApiProperty({ example: '["teste.com"]' })
   @IsArray()
   @IsString({ each: true })
   readonly domains: string[];
 
+  @ApiProperty({ example: 'Universidade de teste' })
   @IsString()
   readonly name: string;
 }
